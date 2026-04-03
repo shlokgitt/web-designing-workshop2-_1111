690a9bbc3e296994c727460bc9f2a27147aca3b2
Shlok Srivastava <shloksri003@gmail.com>
2026-04-04 00:34:46 +0530

    done

---
+++ JavaScript/experiment 5/script.js
@@ -0,0 +1,15 @@
+let employees={
+    name:"alice",
+    salary:5000,
+    dept:"hr"
+};
+console.log(employees);
+function addemployee(){
+    let emp={
+        name:document.getElememntById("name"),
+        id:document.getElementById("id"),
+        salary: document.getElememntById()
+
+    }
+
+}
