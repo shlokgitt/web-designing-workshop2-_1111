1509301f65a1d81e54ff298b83d50f5ed0084142
Shlok Srivastava <shloksri003@gmail.com>
2026-04-13 15:13:36 +0530

    solution done

---
+++ mid_term1/app1.js
@@ -0,0 +1,10 @@
+let number1 = parseInt(prompt("enter the first number "));
+let number2 = parseInt(prompt("enter the second number "));
+let addition = number1 + number2;
+let difference = number1 - number2;
+let product = number1 * number2;
+ 
+document.write("<p>RESULT</p>");
+document.write("<p>difference: " + difference + "</p>");
+document.write("<p>addition: " + addition + "</p>");
+document.write("<p>product: " + product + "</p>");
