286fb385853057289a75f9dd94fb5f53e36c9eae
Shlok Srivastava <shloksri003@gmail.com>
2026-05-17 15:17:42 +0530

    age section alsoa added

--- Exp-7/src/App.jsx
+++ Exp-7/src/App.jsx
@@ -5,15 +5,17 @@ function App() {
 <Student name="Aman" marks={92} />
       <Student name="Priya" marks={78} />
       <Student name="Rahul" marks={85} />
+      <Student name="deep" age={22}/>
     </div>
   );
 }
 // Student Component
-function Student({ name, marks }) {
+function Student({ name, marks,age  }) {
   return (
     <div>
       <h2>{name}</h2>
       <p>Marks: {marks}</p>
+      <p>Marks: {age}</p>
     </div>
   );
 }
